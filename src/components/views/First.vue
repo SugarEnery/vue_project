5f28fd6ae72f19566e171745575806566843f48c
palao_web_admin <17600409331@163.com>
2020-11-20 17:52:53 +0800

    add增加一个项目

---
+++ src/components/views/First.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="first-app">
+    {{ msg }}
+    <confirm text="注册" @message="getMessage"></confirm>
+    <router-link to="/second">去第二个页面</router-link>
+  </div>
+</template>
+
+<script>
+import Confirm from '../sub/Confirm'
+export default {
+  name: 'First',
+  components: {
+    Confirm
+  },
+  data () {
+    return {
+      msg: 'come on!'
+    }
+  },
+  methods: {
+    getMessage (val) {
+      alert(val)
+    }
+  }
+}
+</script>
+
+<style scoped >
+.first-app {
+  font-size: 30px
+}
+</style>
---
+++ src/components/views/Second.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="first-app">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    name:'First',
+    data(){
+        return {
+            msg:"come on!"
+        }
+    },
+    methods:{
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
