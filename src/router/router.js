dadd3558bd906d40025bee2cb9e1fbc84d87cda7
palao_web_admin <17600409331@163.com>
2020-12-17 16:42:19 +0800

    fix更新

---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import HelloWorld from '@/components/HelloWorld'
+import First from '@/components/views/First'
+export default [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/first',
+    name: 'First',
+    component: First
+  }
+]
